3be937689b28616cef5fc96fe89337013ce87136
Бабицкий Александр <ababitsky@rzdit.ru>
2022-11-15 16:15:46 +0300

    fix avatar field ob back

--- app/javascript/modules/Auth/pages/Users.vue
+++ app/javascript/modules/Auth/pages/Users.vue
@@ -1,114 +1,112 @@
 <template>
-    <MyNavbar/>
-    <div class="wrapper">
-        <div style="background-attachment:scroll">
-            <button @click="dialogVisiable" class="btn">Создать пользователя</button>
-        <!-- <div>{{todoStore.token}}</div> -->
-        <div v-if="todoStore.show == true" class="dialog" @click="dialogUnvisiable"> 
-            <div  @click.stop class="dialog-content">
-            <div class="input-block">
-                <div class="input-content">
-                    <div class="block">
-                        <label>Имя:</label>
-                        <my-input v-model="todoStore.first_name" class="input"/>
-                    </div>
+  <MyNavbar/>
+  <div class="wrapper">
+    <div style="background-attachment:scroll">
+      <button @click="dialogVisiable" class="btn">Создать пользователя</button>
+      <!-- <div>{{todoStore.token}}</div> -->
+      <div v-if="todoStore.show == true" class="dialog" @click="dialogUnvisiable">
+        <div @click.stop class="dialog-content">
+          <div class="input-block">
+            <div class="input-content">
+              <div class="block">
+                <label>Имя:</label>
+                <my-input v-model="todoStore.first_name" class="input"/>
+              </div>
 
-                    <div class="block">
-                        <label>Фамилия:</label>
-                        <my-input v-model="todoStore.last_name" class="input"/>
-                    </div>
-                    <div class="block">
-                        <label>Отчество:</label>
-                        <my-input v-model="todoStore.middle_name" class="input"/>
-                    </div>
-                    <div class="block">
-                        <label>Дата рождения:</label>
-                        <my-input v-model="todoStore.birthday" class="input"/>
-                    </div>
-                    <div class="block">
-                        <label>Страна:</label>
-                        <my-input v-model="todoStore.location" class="input"/>
-                    </div>
-                    <div class="block">
-                    <label>Номер:</label>
-                    <my-input v-model="todoStore.phone" class="input"/>
-                    </div>
-                    <div class="block">
-                        <label>Телеграм:</label>
-                        <my-input v-model="todoStore.telegram" class="input"/>
-                    </div>
-                </div>
-                
-                <div class="input-content">
-                    <div class="block">
-                        <label>Должность:</label>
-                        <my-input v-model="todoStore.position" class="input"/>
-                    </div>
+              <div class="block">
+                <label>Фамилия:</label>
+                <my-input v-model="todoStore.last_name" class="input"/>
+              </div>
+              <div class="block">
+                <label>Отчество:</label>
+                <my-input v-model="todoStore.middle_name" class="input"/>
+              </div>
+              <div class="block">
+                <label>Дата рождения:</label>
+                <my-input v-model="todoStore.birthday" class="input"/>
+              </div>
+              <div class="block">
+                <label>Страна:</label>
+                <my-input v-model="todoStore.location" class="input"/>
+              </div>
+              <div class="block">
+                <label>Номер:</label>
+                <my-input v-model="todoStore.phone" class="input"/>
+              </div>
+              <div class="block">
+                <label>Телеграм:</label>
+                <my-input v-model="todoStore.telegram" class="input"/>
+              </div>
+            </div>
 
-                <div class="block">
-                    <label>Почта:</label>
-                    <my-input v-model="todoStore.email" class="input"/>
-                </div>
-                <div class="block">
-                    <label>Логин:</label>
-                    <my-input v-model="todoStore.login" class="input"/>
-                </div>
-                <div class="block">
-                    <label>Пароль:</label>
-                    <my-input v-model="todoStore.password" class="input"/>
-                </div> 
-                <div class="block">
-                    <label>Описание:</label>
-                    <textarea v-model="todoStore.bio" class="textarea" cols="50" rows="10" ></textarea>
-                </div> 
-                <div class="block">
-                    <h4 style="margin:0">Аватар</h4>
-                    <!-- <label for="file" class="avatar-img">Загрузить фотографию</label> -->
-                    <input type="file" id="file" class="avatar-img" name="photo" multiple
-                     accept="image/*,image/jpeg">
-                </div> 
-                </div>
+            <div class="input-content">
+              <div class="block">
+                <label>Должность:</label>
+                <my-input v-model="todoStore.position" class="input"/>
+              </div>
+
+              <div class="block">
+                <label>Почта:</label>
+                <my-input v-model="todoStore.email" class="input"/>
+              </div>
+              <div class="block">
+                <label>Логин:</label>
+                <my-input v-model="todoStore.login" class="input"/>
+              </div>
+              <div class="block">
+                <label>Пароль:</label>
+                <my-input v-model="todoStore.password" class="input"/>
+              </div>
+              <div class="block">
+                <label>Описание:</label>
+                <textarea v-model="todoStore.bio" class="textarea" cols="50" rows="10"></textarea>
+              </div>
+              <div class="block">
+                <h4 style="margin:0">Аватар</h4>
+                <input type="file" id="file" class="avatar-img" name="photo" multiple
+                       accept="image/*,image/jpeg">
+              </div>
             </div>
-                
-             <button class="btn" @click="todoStore.createUser">Создать</button>                             
-            </div>
- 
+          </div>
+
+          <button class="btn" @click="todoStore.createUser">Создать</button>
         </div>
 
-        <button class="btn">Список пользователей</button>
-        <div class="all-user">
-            <div v-for="user in todoStore.users" class="user-block">
-                <div class="user-section"></div>
-                <div class="user-section">
-                    <p>Имя: {{user.first_name}}</p>
-                    <p>Фамилия: {{user.last_name}}</p>
-                    <p>Отчество: {{user.middle_name}}</p>
-                    <p>Страна: {{user.location}}</p>
-                    <p>Телеграм:{{user.telegram}}</p>
-                    <p>Номер:{{user.phone}}</p>
-                    <p>Почта:{{user.email}}</p>                    
-                </div>
+      </div>
 
-                
+      <button class="btn">Список пользователей</button>
+      <div class="all-user">
+        <div v-for="user in todoStore.users" class="user-block">
+          <div class="user-section"></div>
+          <div class="user-section">
+            <p>Имя: {{ user.first_name }}</p>
+            <p>Фамилия: {{ user.last_name }}</p>
+            <p>Отчество: {{ user.middle_name }}</p>
+            <p>Страна: {{ user.location }}</p>
+            <p>Телеграм:{{ user.telegram }}</p>
+            <p>Номер:{{ user.phone }}</p>
+            <p>Почта:{{ user.email }}</p>
+          </div>
 
-            </div>
-        </div>
 
         </div>
-        
-   
+      </div>
+
     </div>
-    
+
+
+  </div>
+
 </template>
 <script setup>
 
 import {useTodoStore} from '@/store/store'
-import { onMounted } from 'vue';
+import {onMounted} from 'vue';
 
 const todoStore = useTodoStore()
 const updateUser = onMounted(() => {
-    todoStore.getAllUsers
-    })
+  todoStore.getAllUsers
+})
 
 // const users = todoStore.users
 
@@ -128,131 +126,145 @@ const updateUser = onMounted(() => {
 // })
 
 const dialogVisiable = () => {
-    todoStore.show = true
+  todoStore.show = true
 }
 
 const dialogUnvisiable = () => {
-    todoStore.show = false
+  todoStore.show = false
 }
-
 
 
 </script>
 <script>
 import MyNavbar from '@/components/UI/MyNavbar.vue'
 import MyInput from '@/components/UI/MyInput.vue'
-export default{
-    components:{
-        MyNavbar,
-        MyInput
-    }
+
+export default {
+  components: {
+    MyNavbar,
+    MyInput
+  }
 }
 </script>
 <style scoped>
 .wrapper {
   width: 100%;
   height: 100%;
-  background-attachment:fixed;
+  background-attachment: fixed;
   top: 65px;
   left: 0px;
   background: #EEF1F6;
-  padding: 100px;    
+  padding: 100px;
 }
-.dialog{
-    padding: 50px;
-    background: rgba(0,0,0,0.5);
-    position: fixed;
-    top:0;
-    bottom: 0;
-    left: 0;
-    right:0;
+
+.dialog {
+  padding: 50px;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
 }
-.dialog-content{
-    background: white;
-    border-radius: 12px;
-    width: 650px;
-    padding: 50px 0 50px 50px;
-    transition: all 0.8s ease;    
+
+.dialog-content {
+  background: white;
+  border-radius: 12px;
+  width: 650px;
+  padding: 50px 0 50px 50px;
+  transition: all 0.8s ease;
 
 
 }
-.dialog-content:hover{
-    background: rgb(226, 226, 226);
-    border-radius: 12px;
-    width: 650px;
-    padding: 50px 0 50px 50px;
-    border: 1px solid rgba(0, 0, 0, 0.158);
-    transition: all 0.8s ease;    
+
+.dialog-content:hover {
+  background: rgb(226, 226, 226);
+  border-radius: 12px;
+  width: 650px;
+  padding: 50px 0 50px 50px;
+  border: 1px solid rgba(0, 0, 0, 0.158);
+  transition: all 0.8s ease;
 
 }
-.input-block{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+
+.input-block {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
-.input-content{
-    display: flex;
-    flex-direction: column;
-    margin:0;
-    width: 50%;
+
+.input-content {
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  width: 50%;
 }
-.input{
-    max-width: 240px;
-    /* margin: 20px 0px 20px 15px; */
+
+.input {
+  max-width: 240px;
+  /* margin: 20px 0px 20px 15px; */
 }
+
 .btn {
-    min-width: 98px;
-    min-height: 44px;
-    background: #2376b6;;
-    border: none;
-    border-radius: 10px;
-    color: white;
-    font: 700 16px / 30px 'Montserrat';
-    padding: 10px;
-    cursor: pointer;
-    
-  }
-.block{
-    margin: 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
+  min-width: 98px;
+  min-height: 44px;
+  background: #2376b6;;
+  border: none;
+  border-radius: 10px;
+  color: white;
+  font: 700 16px / 30px 'Montserrat';
+  padding: 10px;
+  cursor: pointer;
+
 }
-label{
-    margin:0
+
+.block {
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 }
-.textarea{
-    max-width: 240px;
-    max-height: 130px;
-    margin:15px 0;
-    border: 1px solid #F0EFF2;
-    border-radius: 10px;
-    padding:10px
+
+label {
+  margin: 0
 }
-.avatar-img{
-    border: 1px solid #F0EFF2;
-    padding:10px;
-    margin:7px 0;
-    max-width: 240px;
-    border-radius: 10px;
+
+.textarea {
+  max-width: 240px;
+  max-height: 130px;
+  margin: 15px 0;
+  border: 1px solid #F0EFF2;
+  border-radius: 10px;
+  padding: 10px
 }
-.all-user{
-    width: 90%;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    
+
+.avatar-img {
+  border: 1px solid #F0EFF2;
+  padding: 10px;
+  margin: 7px 0;
+  max-width: 240px;
+  border-radius: 10px;
 }
-.user-block{
-    margin:20px 0;
-    width: 50%;
-    height: 400px;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    border:1px solid black;    
+
+.all-user {
+  width: 90%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
 }
-.user-section{
-    width: 45%;
+
+.user-block {
+  margin: 20px 0;
+  width: 50%;
+  height: 400px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  border: 1px solid black;
+}
+
+.user-section {
+  width: 45%;
 }
 </style>
--- app/javascript/store/store.js
+++ app/javascript/store/store.js
@@ -1,103 +1,97 @@
-import { defineStore } from 'pinia'
+import {defineStore} from 'pinia'
 import axios from 'axios'
 
 export const useTodoStore = defineStore({
-  id: 'todo',
-  state: () => ({ 
-     
-    user:null,
-    users:[],
-    show:false,
-    token:localStorage.getItem('token'),
+    id: 'todo',
+    state: () => ({
 
-    birthday:"",
-    position:"",
-    first_name: "",
-    middle_name: "",
-    last_name: "",
-    location: "",
-    telegram: "",
-    login:"",
-    password:"",
-    phone: "",
-    position: "",
-    email:"",
-    bio: "",
-    id: "",
-    avatar:"",
-    
+      user: null,
+      users: [],
+      show: false,
+      token: localStorage.getItem('token'),
+
+      birthday: "",
+      position: "",
+      first_name: "",
+      middle_name: "",
+      last_name: "",
+      location: "",
+      telegram: "",
+      login: "",
+      password: "",
+      phone: "",
+      email: "",
+      bio: "",
+      id: "",
+      avatar: "",
+
     }),
     getters: {
-        async getUser(){
-            try {
-                const response = await axios.get('/api/user/session', {
-                  headers: {
-                    Authorization: localStorage.getItem('token')
-                  }
-                });
-                console.log(response)
-                return this.user = response.data
-                
+      async getUser() {
+        try {
+          const response = await axios.get('/api/user/session', {
+            headers: {
+              Authorization: localStorage.getItem('token')
+            }
+          });
+          console.log(response)
+          return this.user = response.data
 
-              } catch (e) {
-                console.error(e)
-              }
+
+        } catch (e) {
+          console.error(e)
+        }
+
+      }
+      ,
+
+      async getAllUsers() {
+        try {
+          const resp = await axios.get('/api/user/users', {
+            headers: {
+              Authorization: localStorage.getItem('token')
+            }
+          });
+          console.log(resp)
+          return this.users = resp.data
+
+
+        } catch (e) {
+          console.error(e)
+        }
+
+      }
+    },
+    actions: {
+      async createUser() {
+        this.show = false
+        try {
+          const user = {
+            first_name: this.first_name,
+            middle_name: this.middle_name,
+            last_name: this.last_name,
+            location: this.location,
+            phone: this.phone,
+            telegram: this.telegram,
+            birthday: this.birthday,
+            position: this.position,
+            password: this.password,
+            bio: this.bio,
+            login: this.login,
+            avatar: this.avatar
 
           }
-          ,
-        
-        async getAllUsers(){
-          try {
-              const resp = await axios.get('/api/user/users', {
-                headers: {
-                  Authorization: localStorage.getItem('token')
-                }
-              });
-              console.log(resp)
-              return this.users = resp.data
-              
-              
+          const test = await axios.post('/api/user/users', {user: {...user}}, {
+            headers: {
+              Authorization: localStorage.getItem('token')
+            },
+          });
 
-            } catch (e) {
-              console.error(e)
-            }
-
+          console.log(test)
+        } catch (e) {
+          console.error(e)
         }
-        },
-    actions:{
-        async createUser(){
-            this.show = false
-            try{
-                const user = {
-                    first_name: this.first_name,
-                    middle_name: this.middle_name,
-                    last_name: this.last_name,
-                    location: this.location,
-                    phone: this.phone,
-                    telegram: this.telegram,
-                    birthday: this.birthday,
-                    position: this.position,
-                    password: this.password,
-                    bio: this.bio,
-                    login:this.login,
-                    password:this.password,
-                    avatar: this.avatar
-                    
-                  }
-                const test = await axios.post('/api/user/users',{user: {...user}}, {
-                    headers: {
-                      Authorization: localStorage.getItem('token')
-                    },
-                  });
-              
-              console.log(test)
-            }
-      
-            catch (e) {
-                console.error(e)
-              }
-        }
+      }
     }
-
-    }
+  }
 )
